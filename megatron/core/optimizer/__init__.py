5b5eba75fdf8ec4ba9b9ed1736b8dc9f19cfe2b7
Honor4Lj <lijunx16@outlook.com>
2026-04-13 09:11:24 +0800

    [train]: fix the bug of engram loss and log param norm. (#24)
    Fix the bug of engram.
    1. Fix param buffer is set error for the engram independent optimizer.
    2. Fix the param norm of independent optimizer is calculated
    incorrectly.
    3. Add nonsupport feature checking for engram.

--- megatron/core/optimizer/__init__.py
+++ megatron/core/optimizer/__init__.py
@@ -581,7 +581,7 @@ def get_megatron_optimizer(
     mp_group = [mp_group] if not isinstance(mp_group, list) else mp_group
     model_parallel_rank = mp_group[0].rank()
     engram_dp_group = process_groups['engram_dp_group']
-    engram_embed_group = process_groups['engram_embed_group']
+    engram_mp_group = process_groups['engram_mp_group']
     engram_dp_group_gloo = process_groups['engram_dp_group_gloo']
     ########## FlagScale End ##########
     expt_tp_pp_group = process_groups['expt_tp_pp_group']
@@ -681,7 +681,7 @@ def get_megatron_optimizer(
         model_chunk_offset=0,
         config=config,
         config_overrides=config_overrides,
-        filter_fn=lambda g: g['is_expert_parallel'],
+        filter_fn=lambda g: g['is_expert_parallel'] and not g['is_engram_parallel'],
         buffer_name='expert_parallel_buffers',
     )
     if dump_param_to_param_group_map is not None:
@@ -722,7 +722,7 @@ def get_megatron_optimizer(
         config=config,
         config_overrides=config_overrides,
         filter_fn=lambda g: g['is_engram_parallel'],
-        buffer_name='engram_parallel_buffers',
+        buffer_name='engram_embedding_buffers',
     )
     if dump_param_to_param_group_map is not None:
         for param_group in engram_param_groups:
@@ -731,7 +731,7 @@ def get_megatron_optimizer(
                 param_to_param_group[param_name] = param_group_id
             param_group_id += 1
     if len(engram_param_groups) > 0:
-        model_parallel_rank = get_pg_rank(engram_embed_group)
+        model_parallel_rank = get_pg_rank(engram_mp_group)
 
         # Pass Gloo process groups into optimizer only if needed.
         if use_gloo_process_groups:
@@ -745,11 +745,11 @@ def get_megatron_optimizer(
                 model_chunks=model_chunks,
                 param_groups=engram_param_groups,
                 per_model_buffers=engram_buffers,
-                model_parallel_group=engram_embed_group,
+                model_parallel_group=engram_mp_group,
                 data_parallel_group=engram_dp_group,
                 data_parallel_group_gloo=engram_data_parallel_group_gloo,
                 data_parallel_group_idx=model_parallel_rank,
-                intra_dist_opt_group=None,
+                intra_dist_opt_group=intra_dist_opt_group,
                 distributed_optimizer_instance_id=distributed_optimizer_instance_id,
             )
         )
